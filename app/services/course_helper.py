8a8d4f2c79dd3f5c8801f2aa198f8d1f2162cb21
Lamonte H <lamonteh2@gmail.com>
2023-09-23 16:52:40 -0500

    Fixed shadowing

--- app/services/course_helper.py
+++ app/services/course_helper.py
@@ -1,10 +1,10 @@
 # filter our dummy course data until we implement actual database models
-def filter_courses(uid: int, course_data: list[dict], courses: list[dict], type: int):
+def filter_courses(uid: int, course_data: list[dict], courses: list[dict], ctype: int):
     found = []
     for data in course_data:
 
         test_id = data.get("student_id", None)
-        if type == 1:
+        if ctype == 1:
             test_id = data.get("teacher_id", None)
 
         if test_id != uid:
@@ -18,9 +18,9 @@ def filter_courses(uid: int, course_data: list[dict], courses: list[dict], type:
     return found
 
 
-def get_course(id: int, courses: list[dict]):
+def get_course(cid: int, courses: list[dict]):
     for course in courses:
-        if id == course.get("id", None):
+        if cid == course.get("id", None):
             return course
     return None
 
@@ -31,15 +31,15 @@ def course_exists(current: dict, courses: list[dict]):
     return False
 
 def course_students(current: dict, cdata: list[dict], students: list[dict]):
-    id = current.get("id")
-    if not id:
+    c_id = current.get("id")
+    if not c_id:
         return None
 
     found = []
 
     for data in cdata:
         cid = data.get("course_id")
-        if cid != id:
+        if cid != c_id:
             continue
         student_id = data.get("student_id")
         if student_id:
