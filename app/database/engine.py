d8ba15f6fd933d913d1b5af06545b293fba7b6ff
Lamonte H <lamonteh2@gmail.com>
2023-10-03 20:14:17 -0500

    made a change to mysql variable to not conflict with docker-mysql container

--- app/database/engine.py
+++ app/database/engine.py
@@ -6,9 +6,9 @@ from . import models # don't remove this, it creates the tables autoloading them
 
 db_env = dotenv_values('.mysql.env')
 mysql_db = db_env.get("MYSQL_DATABASE")
-mysql_host = db_env.get("MYSQL_HOST")
-mysql_user = db_env.get("MYSQL_USER")
 mysql_pass = db_env.get("MYSQL_ROOT_PASSWORD")
+mysql_host = db_env.get("MYSQLDB_HOST")
+mysql_user = db_env.get("MYSQLDB_USER")
 sql_engine = 'mysql://%s:%s@%s/%s' %(mysql_user, quote(mysql_pass), mysql_host, mysql_db)
 
 engine = create_engine(sql_engine, echo=True)
